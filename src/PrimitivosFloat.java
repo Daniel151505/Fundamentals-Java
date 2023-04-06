fe0eb608fdb9e54433fed7015ea8c590d38cb970
Daniel151505 <daniel150520011@gmail.com>
2023-04-05 22:08:03 -0500

    Primitivos numéricos de punto flotante

---
+++ src/PrimitivosFloat.java
@@ -0,0 +1,6 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+        float realFloat = 2.12e3f; //2120f;
+        System.out.println("realFloat = " + realFloat);
+    }
+}

c992617caf861c7ad12fef438940934ed719aaf5
Daniel151505 <daniel150520011@gmail.com>
2023-04-05 20:02:22 -0500

    Ejemplos de Variables

---
+++ src/HolaMundo.java
@@ -0,0 +1,20 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+        
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor) {
+            System.out.println("numero = " + numero);
+            numero2= 15;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "20";
+    }
+}

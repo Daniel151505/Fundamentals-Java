e4e53a9db952cd457f7caf02dfbffa9ec0bb5523
Daniel151505 <daniel150520011@gmail.com>
2023-04-13 22:33:57 -0500

    Ejemplo buscar con sentencias for anidadas y etiquetas

---
+++ src/SentenciaBucleEtiquetasBuscar.java
@@ -0,0 +1,24 @@
+public class SentenciaBucleEtiquetasBuscar {
+    public static void main(String[] args) {
+        String frase = "tres tristes trigotigres tragan trigo en un trigo trigo trigaltrigotrigotrigotrigo";
+        String palabra = "trigo";
+
+        int maxFrase = frase.length();
+        int maxPalabra = palabra.length();
+
+        int cantidad = 0;
+        char letra = 'g';
+
+        buscar:
+        for (int i = 0; i < maxFrase; i++) {
+            int k = i;
+            for (int j = 0; j < maxPalabra; j++) {
+                if(frase.charAt(k++) != palabra.charAt(j)){
+                    continue buscar;
+                }
+            }
+            cantidad++;
+        }
+        System.out.println("Encontrado = " + cantidad + " veces la palabra " + palabra + " en la frase");
+    }
+}

2aa531951ab2b9cb3ca41692cad91502adb52857
Daniel151505 <daniel150520011@gmail.com>
2023-04-20 16:56:49 -0500

    Línea de comando compilar y ejecutar con argumentos

---
+++ src/ArgumentosLineaComando.java
@@ -0,0 +1,18 @@
+//Abrir terminal
+//Ingresar al src
+// ejecutar le siguiente comando: javac ArgumentosLineaComando.java
+// ejecutar el siguiente comando con los siguiente parametros: java ArgumentosLineaComando Andres pepe
+
+public class ArgumentosLineaComando {
+    public static void main(String[] args) {
+
+        if (args.length  == 0) {
+            System.out.println("Debe ingresar argumentos!");
+            System.exit(-1);
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argumentos nro " + i + ": " + args[i]);
+        }
+    }
+}

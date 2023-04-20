9078f7a2841919092525af6809043e55333cfc2f
Daniel151505 <daniel150520011@gmail.com>
2023-04-20 17:48:39 -0500

    Adding changes

--- src/ArgumentosLineaComando.java
+++ src/ArgumentosLineaComando.java
@@ -7,12 +7,12 @@ public class ArgumentosLineaComando {
     public static void main(String[] args) {
 
         if (args.length  == 0) {
-            System.out.println("Debe ingresar argumentos!");
+            System.out.println("Debe ingresar argumentos o parámetros");
             System.exit(-1);
         }
 
         for (int i = 0; i < args.length; i++) {
-            System.out.println("Argumentos nro " + i + ": " + args[i]);
+            System.out.println("Argumentos n°" + i + ": " + args[i]);
         }
     }
 }

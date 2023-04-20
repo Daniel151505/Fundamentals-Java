3d143765ff59d7f6cdc0d7e69fade286bfd21e0c
Daniel151505 <daniel150520011@gmail.com>
2023-04-20 11:41:39 -0500

    La clase Math - trigonometría

--- src/EjemploClaseMath.java
+++ src/EjemploClaseMath.java
@@ -27,5 +27,40 @@ public class EjemploClaseMath {
         // Rendondeo
         long entero = Math.round(3.5);
         System.out.println("entero = " + entero);
+
+        double exp = Math.exp(1);
+        System.out.println("exp = " + exp);
+        
+        double log = Math.log(10);
+        System.out.println("log = " + log);
+
+        double potencia = Math.pow(10, 3);
+        System.out.println("potencia = " + potencia);
+
+        double raiz = Math.sqrt(5);
+        System.out.println("raiz = " + raiz);
+
+        // radianes a grados
+        double grados = Math.toDegrees(1.57);
+        grados = Math.round(grados);
+        System.out.println("convertir de radiande a grados = " + grados);
+
+        // grados a radianes
+        double radianes = Math.toRadians(98.00);
+        System.out.println("convertir grados a radianes = " + radianes);
+
+        // seno
+        System.out.println("sin(90) = " + Math.sin(radianes));
+
+        // coseno
+        System.out.println("cos(90) = " + Math.cos(radianes));
+
+        radianes = Math.toRadians(180.00);
+
+        System.out.println("cos(180) = " + Math.cos(radianes));
+
+        radianes = Math.toRadians(0.00);
+
+        System.out.println("cos(0) = " + Math.cos(radianes));
     }
 }

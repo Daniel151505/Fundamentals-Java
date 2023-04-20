4b8d693db00653d45d6373907ac4a1ce2ad2aad2
Daniel151505 <daniel150520011@gmail.com>
2023-04-20 11:48:59 -0500

    Método random() para generar números aleatorios

---
+++ src/EjemploClaseMathRandom.java
@@ -0,0 +1,18 @@
+public class EjemploClaseMathRandom {
+    public static void main(String[] args) {
+
+        String[] colores = {"azul", "amarillo", "rojo", "negro", "verde", "amarillo"};
+
+        double random = Math.random();
+        System.out.println("random = " + random);
+        random *= colores.length;
+
+        System.out.println("random = " + random);
+        
+        random = Math.floor(random);
+        System.out.println("random = " + random);
+
+        System.out.println("colores = " + colores[(int) random]);
+
+    }
+}
